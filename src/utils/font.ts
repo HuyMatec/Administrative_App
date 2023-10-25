6daafade715d9c38f5f4d29d3e9be461bb408dde
HuyMatec <tranhoanghuy02.11.2003@gmail.com>
2023-10-25 23:48:47 +0700

    first commit

---
+++ src/utils/font.ts
@@ -0,0 +1,23 @@
+export type fontType = {
+  Poppins_Black: string;
+  Poppins_Bold: string;
+  Poppins_ExtraBold: string;
+  Poppins_ExtraLight: string;
+  Poppins_Light: string;
+  Poppins_Medium: string;
+  Poppins_Regular: string;
+  Poppins_SemiBold: string;
+  Poppins_Thin: string;
+};
+
+export const fontFamilySetup: fontType = {
+  Poppins_Black: 'Poppins-Black',
+  Poppins_Bold: 'Poppins-Bold',
+  Poppins_ExtraBold: 'Poppins-ExtraBold',
+  Poppins_ExtraLight: 'Poppins-ExtraLight',
+  Poppins_Light: 'Poppins-Light',
+  Poppins_Medium: 'Poppins-Medium',
+  Poppins_Regular: 'Poppins-Regular',
+  Poppins_SemiBold: 'Poppins-SemiBold',
+  Poppins_Thin: 'Poppins-Thin',
+};
